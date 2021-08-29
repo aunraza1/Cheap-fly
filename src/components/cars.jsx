5b3c27ba218d2824077042b857f71c2df73c190a
basilrao <basilrao123@gmail.com>
2021-08-29 20:01:16 +0500

    commit

--- src/components/cars.jsx
+++ src/components/cars.jsx
@@ -12,16 +12,18 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Loader from './loader'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Logo from '../assets/images/logo-removebg-preview.png'
-
-
+import DirectionsCarIcon from '@material-ui/icons/DirectionsCar';
+import CommuteRoundedIcon from '@material-ui/icons/CommuteRounded';
+import LocationOnRoundedIcon from '@material-ui/icons/LocationOnRounded';
+import AttachMoneyRoundedIcon from '@material-ui/icons/AttachMoneyRounded';
 import {
   Grid,
 } from '@material-ui/core/';
-import  CarDialog  from './SharedComponent/CarDialog';
+import CarDialog from './SharedComponent/CarDialog';
 
-const useStyles = makeStyles(theme =>({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(2),
@@ -32,164 +34,166 @@ const useStyles = makeStyles(theme =>({
   },
 }));
 
-function Cars({ getCars, carData, userData,venderData }) {
+function Cars({ getCars, carData, userData, venderData }) {
   const [toggleOpen, setToggleOpen] = useState(false);
   const [bookingValue, setBookingValue] = useState();
   const [bookingData, setBookingData] = useState();
-  const [searchArray,setSearchArray]=useState([])
-  const [message,setMessage]=useState()
-  const [messageColor,setColor]=useState('white')
+  const [searchArray, setSearchArray] = useState([])
+  const [message, setMessage] = useState()
+  const [messageColor, setColor] = useState('white')
   const classes = useStyles();
   useEffect(() => {
     getCars()
   }, [])
 
 
-  const filterValue=(e)=>{
+  const filterValue = (e) => {
 
-    var newArray=carData.filter((item)=>{
-      if(item.hourlyRate <=parseInt(e.target.value)
-       || item.carName.toUpperCase() == e.target.value.toUpperCase()
-        || item.location.toUpperCase()==e.target.value.toUpperCase()){
+    var newArray = carData.filter((item) => {
+      if (item.hourlyRate <= parseInt(e.target.value)
+        || item.carName.toUpperCase() == e.target.value.toUpperCase()
+        || item.location.toUpperCase() == e.target.value.toUpperCase()) {
         return true
-       
+
       }
-  
+
     })
-    if(newArray.length==false){
-  
-  
-    setMessage("Oops No Match Found!")
-    setColor('red')
-    // setTimeout(()=>{
-    //    setMessage("")
-    // },5000)
+    if (newArray.length == false) {
+
+
+      setMessage("Oops No Match Found!")
+      setColor('red')
+      // setTimeout(()=>{
+      //    setMessage("")
+      // },5000)
     }
-    else{
+    else {
       setMessage("Match Found!")
       setColor('green')
-    //   setTimeout(()=>{
-    //     setMessage("")
-    //  },5000)
+      //   setTimeout(()=>{
+      //     setMessage("")
+      //  },5000)
     }
-  
+
     console.log(newArray)
     setSearchArray(newArray)
-   
-  
+
+
   }
 
 
- 
+
   return (
-    carData.length?
-    <>
-           <div className="mu-logo-area">
-               <Link to="/" className="mu=logo"> 
-              <img height="120"  width="120" src={Logo} />
-              </Link>
-              </div>
-         <div style={{width:700, height:100,margin:'0 auto'}} className="input-group">
-        <input style={{marginTop:40,borderWidth:1,borderColor:'blue',borderRadius:20,height:50}} onChange={(e)=>filterValue(e)} type="search" className="form-control " placeholder="Search by Car,Price,and Location" aria-label="Search" aria-describedby="search-addon" />
+    carData.length ?
+      <div style={{ backgroundColor: '	#F8F8F8', height: '100%' }} >
+        <div className="mu-logo-area">
+          <Link to="/" className="mu=logo">
+            <img height="120" width="120" src={Logo} />
+          </Link>
+        </div>
+        <div style={{ width: 700, height: 100, margin: '0 auto' }} className="input-group">
+          <input style={{ marginTop: 40, borderWidth: 1, borderColor: '#f50', borderRadius: 20, height: 50 }} onChange={(e) => filterValue(e)} type="search" className="form-control " placeholder="Search by Car,Price,and Location" aria-label="Search" aria-describedby="search-addon" />
 
-      </div>
-      <h4 style={{color:messageColor,textAlign:'center',fontFamily:'cursive'}}>{message}</h4>
+        </div>
+        <h4 style={{ color: messageColor, textAlign: 'center', fontFamily: 'cursive' }}>{message}</h4>
 
-      <div className={classes.rootDiv} style={{margin: '3%',}} >
-        <Grid
-          container
-          spacing={2}
-          direction="row"
-          justify="flex-start"
-          alignItems="flex-start"
-        >
+        <div className={classes.rootDiv} style={{ margin: '3%', }} >
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+          >
 
 
 
-  {searchArray.length? searchArray.map((val,i)=>{
-          return(
-            <Grid item xs={12} sm={8} md={3} key={i}>
-            <Card >
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={val?.url}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {`Car Name: ${val?.carName}`}
-                  </Typography>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {`Car Segment: ${val?.carSegment}`}
-                  </Typography>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {`Location: ${val?.location}`}
-                  </Typography>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {`Hourly Rate: ${val?.hourlyRate}`}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary" onClick={()=>{ setBookingValue(searchArray[i]);  setToggleOpen(true)}}>
-                  Rent A Car
-                </Button>
+            {searchArray.length ? searchArray.map((val, i) => {
+              return (
+                <Grid item xs={12} sm={8} md={3} key={i}>
+                  <Card >
+                    <CardActionArea>
+                      <CardMedia
+                        className={classes.media}
+                        image={val?.url}
+                        title="Contemplative Reptile"
+                      />
+                      <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                      <DirectionsCarIcon color='secondary' fontSize='large' /> {val?.carName}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                      
+                      <CommuteRoundedIcon color='secondary' fontSize='large' /> {val?.carSegment}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                      <LocationOnRoundedIcon color='secondary' fontSize='large' /> {val?.location}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                      <AttachMoneyRoundedIcon color='secondary' fontSize='large' /> {`${val?.hourlyRate}/hr`}
+                      </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions style={{backgroundColor: '#f50', alignItems: 'center', justifyContent: 'center'}}>
+                      <Button size="small" color="primary" onClick={() => { setBookingValue(searchArray[i]); setToggleOpen(true) }}>
+                        Rent A Car
+                      </Button>
 
-              </CardActions>
-            </Card>
-            </Grid>
-          )
-          
-        }):carData && carData.map((val, i) => (
-          <Grid item xs={12} sm={8} md={3} key={i}>
-          <Card >
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={val?.url}
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {`Car Name: ${val?.carName}`}
-                </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {`Car Segment: ${val?.carSegment}`}
-                </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {`Location: ${val?.location}`}
-                </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {`Hourly Rate: ${val?.hourlyRate}`}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary" onClick={()=>{ setBookingValue(carData[i]);  setToggleOpen(true)}}>
-                Rent A Car
-              </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
 
-            </CardActions>
-          </Card>
+            }) : carData && carData.map((val, i) => (
+              <Grid item xs={12} sm={8} md={3} key={i}>
+                <Card >
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      image={val?.url}
+                      title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                      <DirectionsCarIcon color='secondary' fontSize='large' /> {val?.carName}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                      
+                      <CommuteRoundedIcon color='secondary' fontSize='large' /> {val?.carSegment}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                      <LocationOnRoundedIcon color='secondary' fontSize='large' /> {val?.location}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                      <AttachMoneyRoundedIcon color='secondary' fontSize='large' /> {`${val?.hourlyRate}/hr`}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions style={{backgroundColor: '#f50', alignItems: 'center', justifyContent: 'center'}}>
+                    <Button size="small" onClick={() => { setBookingValue(carData[i]); setToggleOpen(true) }}>
+                      Rent A Car
+                    </Button>
+
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+
+
           </Grid>
-        ))}
-      
-         
-        </Grid>
-        <CarDialog optionValues={(value) => {
-               setToggleOpen(false)
-               setBookingData(value)
-              
-            }} toggleValue={(value) => { 
-              
-            setToggleOpen(value) 
-            }} openDialog={toggleOpen} bookingValues={bookingValue} userDetails={userData} venderData={venderData} />
-      </div>
-      
+          <CarDialog optionValues={(value) => {
+            setToggleOpen(false)
+            setBookingData(value)
 
-    </>
- :<Loader from="Cars" /> )
+          }} toggleValue={(value) => {
+
+            setToggleOpen(value)
+          }} openDialog={toggleOpen} bookingValues={bookingValue} userDetails={userData} venderData={venderData} />
+        </div>
+
+
+      </div>
+      : <Loader from="Cars" />)
 
 }
 
@@ -201,7 +205,7 @@ const mapDispatchToProps = (dispatch) => ({
 const mapStateToProps = (state) => ({
   carData: state.carData,
   userData: state.loggedUser,
-  venderData:state.venderData,
+  venderData: state.venderData,
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cars)
--- src/components/hotelBooking.jsx
+++ src/components/hotelBooking.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect } from 'react'
-import {getHotelsfromVendors} from '../store/actions'
+import { getHotelsfromVendors } from '../store/actions'
 import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -19,7 +19,7 @@ import {
 } from '@material-ui/core/'
 import { useState } from 'react';
 
-const useStyles = makeStyles(theme =>({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(2),
@@ -30,169 +30,170 @@ const useStyles = makeStyles(theme =>({
   },
 }));
 
-function HotelBooking({getHotelsfromVendors,hotelData,loggedUser,venderData}){
+function HotelBooking({ getHotelsfromVendors, hotelData, loggedUser, venderData }) {
   const classes = useStyles();
-  const [selectedHotel,setSelectedHotel]=useState()
+  const [selectedHotel, setSelectedHotel] = useState()
   const [toggleOpen, setToggleOpen] = useState(false);
-  const [searchArray,setSearchArray]=useState([])
-  const [message,setMessage]=useState()
-  const [messageColor,setColor]=useState('white')
-useEffect(()=>{
+  const [searchArray, setSearchArray] = useState([])
+  const [message, setMessage] = useState()
+  const [messageColor, setColor] = useState('white')
+  useEffect(() => {
     getHotelsfromVendors()
-  
-},[])
+
+  }, [])
 
 
-const filterValue=(e)=>{
+  const filterValue = (e) => {
 
-  var newArray=hotelData.filter((item)=>{
-    if(item.singlePrice <=parseInt(e.target.value)
-     || item.hotelRatings == parseInt(e.target.value)
-      || item.hotelLocation.toUpperCase()==e.target.value.toUpperCase()){
-      return true
-     
+    var newArray = hotelData.filter((item) => {
+      if (item.singlePrice <= parseInt(e.target.value)
+        || item.hotelRatings == parseInt(e.target.value)
+        || item.hotelLocation.toUpperCase() == e.target.value.toUpperCase()) {
+        return true
+
+      }
+
+    })
+    if (newArray.length == false) {
+
+
+      setMessage("Oops No Match Found!")
+      setColor('red')
+      // setTimeout(()=>{
+      //    setMessage("")
+
+      // },5000)
+    }
+    else {
+      setMessage("Match Found!")
+      setColor('green')
+      //   setTimeout(()=>{
+      //     setMessage("")
+
+      //  },5000)
     }
 
-  })
-  if(newArray.length==false){
+    console.log(newArray)
+    setSearchArray(newArray)
 
 
-  setMessage("Oops No Match Found!")
-  setColor('red')
-  // setTimeout(()=>{
-  //    setMessage("")
-    
-  // },5000)
   }
-  else{
-    setMessage("Match Found!")
-    setColor('green')
-  //   setTimeout(()=>{
-  //     setMessage("")
-      
-  //  },5000)
-  }
+  return (
 
-  console.log(newArray)
-  setSearchArray(newArray)
- 
+    hotelData.length ?
+      <div style={{ backgroundColor: '	#F8F8F8', height: '100%' }} >
+        <div className="mu-logo-area">
+          <Link to="/" className="mu=logo">
+            <img height="120" width="120" src={Logo} />
+          </Link>
+        </div>
 
-}
-return (
-  
-  hotelData.length?
-  <>
-          <div className="mu-logo-area">
-               <Link to="/" className="mu=logo"> 
-              <img height="120"  width="120" src={Logo} />
-              </Link>
-              </div>
-      
-      <div style={{width:700, height:100,margin:'0 auto'}} className="input-group">
-        <input style={{marginTop:40,borderWidth:2,borderColor:'blue',borderRadius:20,height:50}} onChange={(e)=>filterValue(e)} type="search" className="form-control " placeholder="Search by Price,Ratings,and Location" aria-label="Search" aria-describedby="search-addon" />
+        <div style={{ width: 700, height: 100, margin: '0 auto' }} className="input-group">
+          <input style={{ marginTop: 40, borderWidth: 2, borderColor: '#f50', borderRadius: 20, height: 50 }} onChange={(e) => filterValue(e)} type="search" className="form-control " placeholder="Search by Price,Ratings,and Location" aria-label="Search" aria-describedby="search-addon" />
+
+        </div>
+        <h4 style={{ color: messageColor, textAlign: 'center', fontFamily: 'cursive' }}>{message}</h4>
+
+
+        <div className={classes.rootDiv} style={{ margin: '3%', }} >
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+
+
+          >
+
+            {searchArray.length ? searchArray.map((v, i) => {
+              return (
+                <Grid item xs={12} sm={12} md={3} key={i}>
+                  <Card >
+                    <CardActionArea>
+                      <CardMedia
+                        className={classes.media}
+                        image={v?.url}
+                        title="Contemplative Reptile"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {`Hotel Name:${v?.hotelName}`}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {`Hotel Ratings:${v?.hotelRatings} ⭐`}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions style={{backgroundColor: '#f50', alignItems: 'center', justifyContent: 'center'}}>
+                      <Button size="small" 
+                        onClick={() => { setSelectedHotel(searchArray[i]); setToggleOpen(true) }}
+                      >
+                        Book Room
+                      </Button>
+
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+
+            }) : hotelData && hotelData.map((v, i) => (
+              <Grid item xs={12} sm={12} md={3} key={i}>
+             
+                <Card >
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      image={v?.url}
+                      title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {`Hotel Name:${v?.hotelName}`}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {`Hotel Ratings:${v?.hotelRatings} ⭐`}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions style={{backgroundColor: '#f50', alignItems: 'center', justifyContent: 'center'}}>
+                    <Button size="small" 
+                      onClick={() => { setSelectedHotel(hotelData[i]); setToggleOpen(true) }}
+                    >
+                      Book Room
+                    </Button>
+
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+
+
+          </Grid>
+          <HotelDialog optionValues={() => {
+            setToggleOpen(false)
+          }} toggleValue={(value) => {
+
+            setToggleOpen(value)
+          }} openDialog={toggleOpen} bookingValues={selectedHotel} userDetails={loggedUser} venderDetail={venderData} />
+        </div>
 
       </div>
-      <h4 style={{color:messageColor,textAlign:'center',fontFamily:'cursive'}}>{message}</h4>
+      :
+      <Loader from="Hotels" />
 
-
-    <div className={classes.rootDiv} style={{margin: '3%',}} >
-      <Grid
-        container
-        spacing={2}
-        direction="row"
-        justify="flex-start"
-        alignItems="flex-start"
-
-
-      >
-      
-        {searchArray.length? searchArray.map((v,i)=>{
-          return(
-                    <Grid item xs={12} sm={12} md={3} key={i}>
-                    <Card >
-                      <CardActionArea>
-                        <CardMedia
-                          className={classes.media}
-                          image={v?.url}
-                          title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="h2">
-                          {`Hotel Name:${v?.hotelName}`}
-                          </Typography>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {`Hotel Ratings:${v?.hotelRatings} ⭐`}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                      <CardActions>
-                        <Button size="small" color="primary" 
-                        onClick={()=>{ setSelectedHotel(searchArray[i]);  setToggleOpen(true)}}
-                        >
-                         Book Room
-                        </Button>
-          
-                      </CardActions>
-                    </Card>
-                    </Grid>
-          )
-          
-        }):hotelData && hotelData.map((v, i) => (
-          <Grid item xs={12} sm={12} md={3} key={i}>
-          <Card >
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={v?.url}
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                {`Hotel Name:${v?.hotelName}`}
-                </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {`Hotel Ratings:${v?.hotelRatings} ⭐`}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary" 
-              onClick={()=>{ setSelectedHotel(hotelData[i]);  setToggleOpen(true)}}
-              >
-               Book Room
-              </Button>
-
-            </CardActions>
-          </Card>
-          </Grid>
-        ))}
-        
-
-      </Grid>
-      <HotelDialog optionValues={() => {
-               setToggleOpen(false)
-            }} toggleValue={(value) => { 
-              
-            setToggleOpen(value) 
-            }} openDialog={toggleOpen} bookingValues={selectedHotel} userDetails={loggedUser} venderDetail={venderData} />
-    </div>
-
-  </>
-  :
- <Loader from="Hotels"/>
- 
-)
+  )
 }
 
-const mapDispatchToProps=(dispatch)=>({
-    getHotelsfromVendors:()=>dispatch(getHotelsfromVendors())
+const mapDispatchToProps = (dispatch) => ({
+  getHotelsfromVendors: () => dispatch(getHotelsfromVendors())
 
 })
 
-const mapStateToProps=(state)=>({
-    hotelData:state.hotelData,
-    loggedUser:state.loggedUser,
-    venderData:state.venderData,
+const mapStateToProps = (state) => ({
+  hotelData: state.hotelData,
+  loggedUser: state.loggedUser,
+  venderData: state.venderData,
 
 })
-export default connect(mapStateToProps,mapDispatchToProps)(HotelBooking)
+export default connect(mapStateToProps, mapDispatchToProps)(HotelBooking)
